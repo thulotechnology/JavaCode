944d56433c2f9c8cf1b84882b3727f4ff7822a3d
Thulo Technology Pvt.Ltd <thulotechnology@gmail.com>
2022-02-15 21:39:59 +0545

    Calc Added

---
+++ src/com/company/SimpleCalc.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class SimpleCalc {
+    public static void main(String[] args) {
+        System.out.println("----------------------------");
+        System.out.println("---------My CALC------------");
+        System.out.println("----------------------------");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        System.out.println("Enter 1 for sum 2 multiply");
+        int userResponse = sc.nextInt();
+
+        switch (userResponse){
+            case 1:
+                System.out.println("The sum is "+(num1+num2));
+                break;
+            case 2:
+                System.out.println("The multiply is "+(num1*num2));
+                break;
+            default:
+                System.out.println("Invaild Option");
+        }
+
+    }
+}
