d0c68304c04926a14e7f8b8dfdbccc58c407e824
Thulo Technology Pvt.Ltd <thulotechnology@gmail.com>
2022-02-16 21:36:03 +0545

    Nested Loop Code Added

--- src/com/company/NestedLoop.java
+++ src/com/company/NestedLoop.java
@@ -3,8 +3,8 @@ package com.company;
 public class NestedLoop {
     public static void main(String[] args) {
         // Print 1 - 10 multiply table.
-        int start =3;
-        int end = 6;
+        int start =1;
+        int end = 10;
         for(int i = start; i<=end; i++){
             for(int j = 1; j<=10; j++ ){
                 System.out.println(i+" * "+j+ " = "+(i*j));
@@ -13,3 +13,6 @@ public class NestedLoop {
         }
     }
 }
+
+// WAP IN JAVA TO PRINT 200 to 100 Using While Loop.
+
